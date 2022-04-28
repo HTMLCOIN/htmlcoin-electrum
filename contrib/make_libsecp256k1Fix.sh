ac0d6be0294625197c74d383e0f221630d271ba8
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-04-28 13:31:42 -0700

    fix conflict on merge issue

--- contrib/make_libsecp256k1Fix.sh
+++ contrib/make_libsecp256k1Fix.sh
@@ -63,6 +63,6 @@ info "Building $pkgname..."
     make install || fail "Could not install $pkgname"
     . "$here/$pkgname/dist/lib/libsecp256k1.la"
     host_strip "$here/$pkgname/dist/lib/$dlname"
-    cp -fpv "$here/$pkgname/dist/lib/$dlname" "Applications/Htmlcoin Electrum.app/Contents/MacOS/" || fail "Could not copy the $pkgname binary to its destination"
+    cp -fpv "$here/$pkgname/dist/lib/$dlname" Applications/Htmlcoin Electrum.app/Contents/MacOS/ || fail "Could not copy the $pkgname binary to its destination"
     info "$dlname has been placed in the inner 'electrum' folder."
 )
